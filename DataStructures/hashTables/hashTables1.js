7adca2d07824d21168eb6a6bdc8dcf5b517637b6
Redyf <mateusalvespereira7@gmail.com>
2023-12-21 19:05:25 -0300

    New exercises

---
+++ DataStructures/hashTables/hashTables1.js
@@ -0,0 +1,15 @@
+let user = {
+  age: 54,
+  name: "Kylie",
+  magic: true,
+  scream: function() {
+    console.log("ahhhhhh!");
+  }
+}
+
+user.age; // O(1)
+user.spell = "abra kadabra"; // O(1)
+user.scream(); // O(1)
+
+const a = new Map()
+const b = new Sets()
